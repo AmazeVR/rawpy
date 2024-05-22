bbccb08693e2c78aafca5b71a948b95aef073837
Steven-Koo <steven@amazevr.com>
2024-05-21 18:19:26 -0700

    Add function to read bayer image

--- rawpy/__init__.py
+++ rawpy/__init__.py
@@ -35,3 +35,15 @@ def imread_frame(pathOrFile, frame_idx):
     else:
         d.open_file_frame(pathOrFile, frame_idx)
     return d
+
+def open_bayer(buffer, raw_width, raw_height, bayer_pattern):
+    """
+    Convenience function that creates a :class:`rawpy.RawPy` instance with frame index,
+    opens the given file, and returns the :class:`rawpy.RawPy` instance for further processing.
+
+    :param str|file pathOrFile: path or file object of RAW image that will be read
+    :rtype: :class:`rawpy.RawPy`
+    """
+    d = RawPy()
+    d.open_bayer(buffer, raw_width, raw_height, bayer_pattern)
+    return d
